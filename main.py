d5694121fcc885bcc56fb923744978faf1804020
toguiker <145672674+toguiker@users.noreply.github.com>
2023-09-22 09:00:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
 #!/usr/bin/env python3
 
-print("hello world\n")
-
-hola = input("Dame un numero")
-
-print("no os cargeis el repo")
-
-
-hola = [1,2,3,4,5]
-
-main =input("Estos es una prueba")
-
 
