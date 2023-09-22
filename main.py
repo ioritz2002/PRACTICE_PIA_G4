20aa886e7c2122d7f53cab0e5da43529522e9c04
ioritz2002 <ioritzurkiri2002@gmail.com>
2023-09-22 10:47:48 +0200

    json file writing works

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
 #!/usr/bin/env python3
 
+import objetos_y_clases
+import json
 
+from datetime import datetime
+
+
+def set_fecha_nacimiento(fecha):
+    try:
+        fecha_nacimiento = datetime.strptime(str(fecha), "%d/%m/%Y")
+        return fecha_nacimiento.strftime("%d/%m/%Y")
+    except Exception:
+        print("Fecha incorrecta")
+
+    
+
+
+def run():
+    data = {}
+
+    persona = objetos_y_clases.Persona()
+    persona.nombre = input("Introduce el nombre\n")
+    persona.apellidos = input("Introduce los apellidos\n")
+    fecha = input("Introduce la fecha de nacimiento en formato dd/mm/aaaa\n")
+    persona.fecha_nacimiento = set_fecha_nacimiento(fecha)
+
+    personas = []
+    with open("personas.json", "r") as archivo:
+        contenido = archivo.read()
+        personas = json.loads(contenido) if contenido else []
+   
+        
+
+    personas.append(persona.__dict__)
+
+    with open("personas.json", "w") as archivo:
+        archivo.write(json.dumps(personas, indent=4))
+
+    
+
+run()
---
+++ objetos_y_clases.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#Esto de aqui es de ioritz
+import datetime
+import json
+class Persona:
+
+    def __init__(self, nombre, apellidos, fecha):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.fecha_nacimiento = fecha
+
+
+    def __init__(self):
+        pass
+
+    
+
+
+    
+    
