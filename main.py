e2bdef0b6d5f490084779080a2396f4579130dba
ioritz2002 <ioritzurkiri2002@gmail.com>
2023-09-25 08:24:18 +0200

    Json writing and reading like as object working

--- main.py
+++ main.py
@@ -17,33 +17,52 @@ def set_fecha_nacimiento(fecha):
 
 
 def run():
+    #Creamos el objeto y le introducimos los datos
     persona = objetos_y_clases.Persona()
     persona.nombre = input("Introduce el nombre\n")
     persona.apellidos = input("Introduce los apellidos\n")
     fecha = input("Introduce la fecha de nacimiento en formato dd/mm/aaaa\n")
     persona.fecha_nacimiento = set_fecha_nacimiento(fecha)
-
+    
+    #Una lista vacia de personas
     personas = []
+
+    #Abrimos el archivo en modo lectura para ver si el archivo esta vacio
+    #o por el contrario hay datos ya introducidos, en caso de que haya datos introducidos
+    #los lee y los guarda en la lista de personas
     with open("personas.json", "r") as archivo:
         contenido = archivo.read()
         personas = json.loads(contenido) if contenido else []
    
-        
-
+    #Añadimos a la lista de personas el objeto de persona creado por nosotros y lo guardamos como diccionario
     personas.append(persona.__dict__)
 
+    #Esto escribe en el json
     with open("personas.json", "w") as archivo:
-        archivo.write(json.dumps(personas, indent=4))
+       archivo.write(json.dumps(personas, indent=4))
 
-
+    #Esto lee el json y lo guarda como strings y por tanto no se puede usar como objeto del tipo persona
     with open("personas.json", "r") as archivo:
         contenido = archivo.read()
         personas = json.loads(contenido) if contenido else []
     
+    personas = []
 
+    #Lee el json y lo guarda en una lista de objetos de tipo Persona para poder utilizarlo
+    with open("personas.json", "r") as archivo:
+        contenido = archivo.read()
+        
+        if contenido:
+            datos = json.loads(contenido)
 
-    for i in personas:
-        print(i)
+            for d in datos:
+                persona = objetos_y_clases.Persona()
+                persona.nombre = d["nombre"]
+                persona.apellidos = d["apellidos"]
+                persona.fecha_nacimiento = d["fecha_nacimiento"]
+
+                personas.append(persona)
     
-
+    persona = personas[0]
+    print(persona.nombre)
 run()
