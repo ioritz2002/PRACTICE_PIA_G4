20b9e8f5dc2c3a566326647e804d5026ff37f8c7
GaizkaLopez <gaizkalopeztx@gmail.com>
2023-09-27 09:05:07 +0200

    a

--- main.py
+++ main.py
@@ -1,3 +1,51 @@
 #!/usr/bin/env python3
 
+import objetos_y_clases
+import json
+import plantilla_metodos
 
+from datetime import datetime
+
+file_name = "personas.json"
+
+
+
+
+#Metodo para setear fecha de nacimento en formato valido para json
+def set_fecha_nacimiento(fecha):
+    try:
+        #Esto lo formatea a un datetime
+        fecha_nacimiento = datetime.strptime(str(fecha), "%d/%m/%Y")
+        #Esto lo formatea a string pero en un formato que se quiera
+        return fecha_nacimiento.strftime("%d/%m/%Y")
+    except Exception:
+        print("Fecha incorrecta")
+
+    
+
+
+def run():
+    #Creamos el objeto y le introducimos los datos
+    persona = objetos_y_clases.Persona()
+    persona.nombre = input("Introduce el nombre\n")
+    persona.apellidos = input("Introduce los apellidos\n")
+    #Fecha en formato string
+    fecha = input("Introduce la fecha de nacimiento en formato dd/mm/aaaa\n")
+    persona.fecha_nacimiento = set_fecha_nacimiento(fecha)
+
+    plantilla_metodos.write(persona, file_name)
+    personas = plantilla_metodos.read(file_name)
+    print(personas)
+
+    personas = []
+    personas = plantilla_metodos.read_objects(file_name)
+    print(personas)
+    print(personas[0].nombre)
+
+    for p in personas:
+        if p.nombre == "xabi":
+            key_value = p.nombre
+            p.nombre = "xabier"
+            plantilla_metodos.update(p, key_value, file_name)
+   
+run()
---
+++ plantilla_metodos.py
@@ -0,0 +1,77 @@
+import json
+import objetos_y_clases
+
+def write(obj, file_name):
+    objs = []
+    objs = read(file_name)
+    objs.append(obj.__dict__)
+    
+    with open(file_name, "w") as archivo:
+        archivo.write(json.dumps(objs, indent=4))
+    pass
+
+#Lee en formato string
+def read(file_name):
+    objs = []
+
+    try:
+
+        with open(file_name, "r") as archivo:
+            contenido = archivo.read()
+            objs = json.loads(contenido) if contenido else []
+        
+        return objs
+    except FileNotFoundError:
+        with open(file_name, "w") as archivo:
+            archivo.write(json.dumps(objs, indent=4))
+
+        return []
+
+#Adaptarlo a las necesidades del software
+#Retorna una lista de objetos de lo que se guarda en python
+def read_objects(file_name):
+    objs = []
+
+    try:
+
+        with open(file_name, "r") as archivo:
+            contenido = archivo.read()
+            
+            if contenido:
+                datos = json.loads(contenido)
+
+                for d in datos:
+                    persona = objetos_y_clases.Persona()
+                    persona.nombre = d["nombre"]
+                    persona.apellidos = d["apellidos"]
+                    persona.fecha_nacimiento = d["fecha_nacimiento"]
+
+                    objs.append(persona)
+        
+        return objs
+    except FileNotFoundError:
+        with open(file_name, "w") as archivo:
+            archivo.write(json.dumps(objs, indent=4))
+
+        return []
+#Modificar en funcion de las necesidades del software
+def delete(obj, file_name):
+    objs = read_objects
+
+    
+
+    pass
+#Actualiza 
+def update(obj, key_value, file_name):
+    with open(file_name, "r") as archivo:
+        datos = json.load(archivo)
+    
+    for persona in datos:
+        if persona["nombre"] == key_value:
+            persona["nombre"] = obj.nombre
+
+    with open(file_name, "w") as archivo:
+        json.dump(datos, archivo, indent=4)
+
+    pass
+
