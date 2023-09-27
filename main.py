e4a42d2a658de3ece4a4e29d76b79b1f97d2db4a
GaizkaLopez <gaizkalopeztx@gmail.com>
2023-09-27 10:00:15 +0200

    Menu_Creado

--- main.py
+++ main.py
@@ -8,43 +8,66 @@ from datetime import datetime
 
 file_name = "personas.json"
 
+out=False
 
+while out!=True:
+    command=input("Introduce una opcion:\n"
+                  "1º Crear\n"
+                  "2º Leer\n"
+                  "3º Actualizar\n"
+                  "4º Borrar\n"
+                  "5º Salir del programa\n")
+    match command:
+        case "1":
+            print("Opcion seleccionada 1:\n")
 
+            def set_fecha_nacimiento(fecha):
+                try:
+                    #Esto lo formatea a un datetime
+                    fecha_nacimiento = datetime.strptime(str(fecha), "%d/%m/%Y")
+                    #Esto lo formatea a string pero en un formato que se quiera
+                    return fecha_nacimiento.strftime("%d/%m/%Y")
+                except Exception:
+                    print("Fecha incorrecta")
 
-#Metodo para setear fecha de nacimento en formato valido para json
+                #Creamos el objeto y le introducimos los datos
+            persona = objetos_y_clases.Persona()
+            persona.nombre = input("Introduce el nombre\n")
+            persona.apellidos = input("Introduce los apellidos\n")
+                #Fecha en formato string
+            fecha = input("Introduce la fecha de nacimiento en formato dd/mm/aaaa\n")
+            persona.fecha_nacimiento = set_fecha_nacimiento(fecha)
 
-def set_fecha_nacimiento(fecha):
-    try:
-        #Esto lo formatea a un datetime
-        fecha_nacimiento = datetime.strptime(str(fecha), "%d/%m/%Y")
-        #Esto lo formatea a string pero en un formato que se quiera
-        return fecha_nacimiento.strftime("%d/%m/%Y")
-    except Exception:
-        print("Fecha incorrecta")
+            plantilla_metodos.write(persona, file_name)
+            personas = plantilla_metodos.read(file_name)
+            print(personas)
+        case "2":
+            print("Opcion seleccionada 2:\n")
+            personas = plantilla_metodos.read(file_name)
+            print(personas)
+        case "3":
+            print("Opcion seleccionada 3:\n")
+            plantilla_metodos.write(persona, file_name)
+            personas = plantilla_metodos.read(file_name)
+            print(personas)
 
-def run():
-    #Creamos el objeto y le introducimos los datos
-    persona = objetos_y_clases.Persona()
-    persona.nombre = input("Introduce el nombre\n")
-    persona.apellidos = input("Introduce los apellidos\n")
-    #Fecha en formato string
-    fecha = input("Introduce la fecha de nacimiento en formato dd/mm/aaaa\n")
-    persona.fecha_nacimiento = set_fecha_nacimiento(fecha)
+            personas = []
+            personas = plantilla_metodos.read_objects(file_name)
+            print(personas)
+            print(personas[0].nombre)
 
-    plantilla_metodos.write(persona, file_name)
-    personas = plantilla_metodos.read(file_name)
-    print(personas)
+            for p in personas:
+                if p.nombre == "xabi":
+                    key_value = p.nombre
+                    p.nombre = "xabier"
+                    plantilla_metodos.update(p, key_value, file_name)
+        case "4":
+            print("Opcion seleccionada 4:\n")
+        case "5":
+            out=True
+            print("Has salido del programa correctamente")
+        case other:
+            print("Error")
+  
 
-    personas = []
-    personas = plantilla_metodos.read_objects(file_name)
-    print(personas)
-    print(personas[0].nombre)
-
-    for p in personas:
-        if p.nombre == "xabi":
-            key_value = p.nombre
-            p.nombre = "xabier"
-            plantilla_metodos.update(p, key_value, file_name)
-   
-run()
 
